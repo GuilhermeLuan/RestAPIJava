3ef286c158ec4717dfce0dfcaad671b3e786965b
Guilherme <guilherme@Guilherme.localdomain>
2024-02-15 19:43:25 -0300

    feat: implementando a camada service

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.springboot.controllers;
 import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import com.example.springboot.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,18 +26,18 @@ public class ProductController implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Autowired
-    ProductRepository productRepository;
+    private ProductService productService;
 
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel); //Faz a conversão de DTO para Model.
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.insert(productModel));
     }
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts(){
-        List<ProductModel> productList = productRepository.findAll();
+        List<ProductModel> productList = productService.findAll();
         if(!productList.isEmpty()){
             for (ProductModel product : productList){
                 UUID id = product.getIdProduct();
@@ -48,7 +49,7 @@ public class ProductController implements Serializable {
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
-        Optional<ProductModel> productO = productRepository.findById(id);
+        Optional<ProductModel> productO = productService.findById(id);
         if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
 
@@ -60,22 +61,22 @@ public class ProductController implements Serializable {
     @PutMapping("/products/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
                                                 @RequestBody @Valid ProductRecordDto productRecordDto) {
-        Optional<ProductModel> productO = productRepository.findById(id);
+        Optional<ProductModel> productO = productService.findById(id);
         if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
         var productModel = productO.get();
         BeanUtils.copyProperties(productRecordDto, productModel);
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+        return ResponseEntity.status(HttpStatus.OK).body(productService.update(id ,productModel));
     }
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id){
-        Optional<ProductModel> productO = productRepository.findById(id);
+        Optional<ProductModel> productO = productService.findById(id);
         if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
         }
-        productRepository.delete(productO.get());
+        productService.deleteById(productO.get().getIdProduct());
 
         return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
     }
---
+++ src/main/java/com/example/springboot/services/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.springboot.services;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductModel> findAll(){
+        return productRepository.findAll();
+    }
+
+    public ProductModel insert(ProductModel productModel) {
+        return  productRepository.save(productModel);
+    }
+
+    public Optional<ProductModel> findById(UUID id){
+        return productRepository.findById(id);
+    }
+
+    public void deleteById(UUID id){
+        productRepository.deleteById(id);
+    }
+
+    public ProductModel update(UUID id, ProductModel productModel){
+        ProductModel product = productRepository.getReferenceById(id);
+        updateData(product, productModel);
+        return productRepository.save(productModel);
+    }
+    public void updateData(ProductModel product, ProductModel obj){
+        product.setName(obj.getName());
+        product.setValue(obj.getValue());
+    }
+
+}
