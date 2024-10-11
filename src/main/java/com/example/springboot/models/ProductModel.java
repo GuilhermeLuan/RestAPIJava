8256dca82b3c806a005a933f58efd8d3715c197c
Guilherme Luan <guilhermeluan@pm.me>
2024-10-11 19:50:00 -0300

    feat: update spring

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -2,20 +2,20 @@ package com.example.springboot.models;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serial;
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Builder
 @Table(name = "TB_PRODUCTS")
-public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+public class ProductModel {
     @Serial
     private static final long serialVersionUID = 1L;
 
